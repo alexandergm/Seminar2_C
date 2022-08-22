3a4c93f624201d92d7eb913853eaceac73471a4f
alexandergm <sasha_261@outlook.com>
2022-08-23 00:21:48 +0300

    Complete Task13

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Загадайте любое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string N = SecondNum(a);
+Console.WriteLine(N);
+
+string SecondNum(int count)
+{
+    string output;
+    if(count > 99)
+    {
+        while (count >= 1000) count /= 10;
+        int c = count % 10;
+        output = "Третья цифра вашего числа = " + c;
+    }
+    else
+    {
+        output = "третьей цифры в этом числе нет";
+    }
+    return output;
+}
