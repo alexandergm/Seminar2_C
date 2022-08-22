1e1e8badeae1dfdb5864e3cf24d55ae94d60bc8d
alexandergm <sasha_261@outlook.com>
2022-08-22 23:59:25 +0300

    Complete Task10

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,4 +1,9 @@
 ﻿Console.WriteLine("Введите 3х значное число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-a = a / 10 % 10;
-Console.WriteLine("Вторая цифра загаданного число это: " + a);
+SecondNum();
+
+void SecondNum()
+{
+    a = a / 10 % 10;
+    Console.WriteLine("Вторая цифра загаданного число это: " + a);
+}
