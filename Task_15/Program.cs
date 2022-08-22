3af0099a74219db3067ea85cb6191d03c08ceed6
alexandergm <sasha_261@outlook.com>
2022-08-23 00:30:11 +0300

    Complete Task15

--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число дня недели. Где 1 - это Понедельник, а 7 - это воскресенье: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(WeekDay(a));
+
+string WeekDay(int count)
+{
+    string day;
+    if(count == 1) day = "Понедельник";
+    else if (count == 2) day = "Вторник";
+    else if (count == 3) day = "Среда";
+    else if (count == 4) day = "Четверг";
+    else if (count == 5) day = "Пятница";
+    else if (count == 6) day = "Суббота";
+    else if (count == 7) day = "Воскресенье";
+    else day = "Вы ввели не верное число";
+    return day;
+}
