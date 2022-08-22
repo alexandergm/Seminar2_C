dae71bf63981fe16c298810b1cbe4c57636fead0
alexandergm <sasha_261@outlook.com>
2022-08-23 01:31:27 +0300

    Complete Extra

--- Extra_Task/Program.cs
+++ Extra_Task/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число программистов в группе");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Sum(a));
+
+string Sum(int b)
+{
+    string result;
+    if (b == 1 || b % 10 == 1 && b % 100 != 11) {
+         result = "В зале " + a + " программист"; 
+    }
+    else if ( b % 100 != 11 && b % 100 != 12 && b % 100 != 13 && b % 100 != 14 && 1 < b % 10 && 5 > b % 10 ){
+        result = "В зале " + a + " программиста"; 
+    }
+    else{
+        result = "В зале " + a + " программистов"; 
+    }
+    return result;
+}
